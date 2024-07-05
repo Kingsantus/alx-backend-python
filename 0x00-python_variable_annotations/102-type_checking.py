591fef6e68d5723b5a1bdfd0cb8072ad02c80517
kingsantus <nkingsantus@gmail.com>
2024-07-05 13:42:58 +0000

    annotation

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -3,19 +3,19 @@
 Task: use mypy to validate the code in this module and apply the necessary
 fixes.
 """
-from typing import Tuple, List
+from typing import Tuple, List, Union
 
-def zoom_array(lst: Tuple, factor: int = 2) -> List:
+def zoom_array(lst: List, factor: Union[int, float] = 2) -> List:
     """TODO: add docstring"""
-    zoomed_in: Tuple = [
+    zoomed_in: List = [
         item for item in lst
         for _ in range(int(factor))
     ]
     return zoomed_in
 
 
-array = (12, 72, 91)
+array = [12, 72, 91]
 
 zoom_2x = zoom_array(array)
 
-zoom_3x = zoom_array(array, 3)
+zoom_3x = zoom_array(array, 3.0)
